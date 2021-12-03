d62bbf417784b58b8a0ffc6b276f5dc40b537ae2
SergeitZ <sergiofranco6@gmail.com>
2021-12-02 21:09:58 -0500

    initial commit

---
+++ src/test/java/com/francofitness/calorietracker/CalorietrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francofitness.calorietracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorietrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
